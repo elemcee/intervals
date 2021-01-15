622c5e65d0c2de93e81db950a625d2c6c79ee302
elemcee <cookout@gmail.com>
2021-01-15 10:21:14 -0800

    Add files via upload

---
+++ intervals.js
@@ -0,0 +1,47 @@
+function hideTwos() {
+    let items = document.getElementsByClassName("two");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button2").classList.toggle("hidden");
+};
+
+function hideThrees() {
+    let items = document.getElementsByClassName("three");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button3").classList.toggle("hidden");
+};
+
+function hideFours() {
+    let items = document.getElementsByClassName("four");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button4").classList.toggle("hidden");
+};
+
+function hideFives() {
+    let items = document.getElementsByClassName("five");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button5").classList.toggle("hidden");
+};
+
+function hideSixes() {
+    let items = document.getElementsByClassName("six");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button6").classList.toggle("hidden");
+};
+
+function hideSevens() {
+    let items = document.getElementsByClassName("seven");
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("hide");
+    };
+    document.getElementById("button7").classList.toggle("hidden");
+};
